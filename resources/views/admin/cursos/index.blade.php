f865ba3b67b8328c92146569b43bc09b4ddfa5fa
Dfluan <dflaphp@gmail.com>
2017-12-06 16:28:58 -0200

    Layout MATERIALIZE / Lista de REGISTRO

---
+++ resources/views/admin/cursos/index.blade.php
@@ -0,0 +1,45 @@
+  @extends('layout.site')
+
+  @section('titulo','Cursos')
+
+
+  @section('conteudo')
+  <div class="container">
+        <h3 class="center">Essa é a views index</h3>
+        <div class="row">
+          <table>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Titulo</th>
+                <th>Descrição</th>
+                <th>imagem</th>
+                <th>valor</th>
+                <th>Publicação</th>
+                <th>Ação</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($registros as $registro)
+              <tr>
+                <td>{{$registro->id}}</td>
+                <td>{{$registro->titulo}}</td>
+                <td>{{$registro->descricao}}</td>
+                <td><img src="{{asset($registro->imagem)}}" alt="{{$registro->titulo}}"> </td>
+                <td>{{$registro->valor}}</td>
+                <td>{{$registro->publicado}}</td>
+                <td>
+                  <a class="btn depp-orange" href="{{route('admin.cursos.editar', $registro->id)}}">Editar</a>
+                  <a class="btn red" href="{{route('admin.cursos.deletar', $registro->id)}}">Deletar</a>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+        <div class="row">
+          <a class="btn blue" href="#">Adicionar</a>
+        </div>
+  </div>
+
+  @endsection
