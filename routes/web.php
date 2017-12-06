dfe937686bce1e922abceaf652b54b5a2caddd14
Dfluan <dflaphp@gmail.com>
2017-12-06 14:12:12 -0200

    Definido as rotas CRUD

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,20 @@ Route::get('/contato/{id?}', ['uses'=>'contatoController@index']);
 Route::post('/contato', ['uses'=>'contatoController@criar']);
 
 Route::put('/contato', ['uses'=>'contatoController@editar']);
+
+Route::get('/admin/cursos',['as'=>'admin.cursos','uses'=>'admin\CursoController@index']);
+Route::get('/admin/adicionar',['as'=>'admin.cursos.adicionar','uses'=>'admin\CursoController@adicionar']);
+Route::post('/admin/salvar',['as'=>'admin.cursos.salavar','uses'=>'admin\CursoController@salvar']);
+Route::get('/admin/editar/{id}',['as'=>'admin.cursos.editar','uses'=>'admin\CursoController@editar']);
+Route::put('/admin/atualizar/{id}',['as'=>'admin.cursos.atualizar','uses'=>'admin\CursoController@atualizar']);
+Route::get('/admin/deletar/{id}',['as'=>'admin.cursos.deletar','uses'=>'admin\CursoController@deletar']);
+
+/*
+
+adicionar
+salvar
+editar
+atualizar
+deletar
+
+*/
